b390de4dd3651273018b396320327c3a2216aa3b
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-06-27 20:20:01 +0700

    Initial Commit

---
+++ SwiftUI1.0/SwiftUI1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI1_0App.swift
+//  SwiftUI1.0
+//
+//  Created by Quốc Huy Nguyễn on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
